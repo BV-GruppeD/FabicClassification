b56e03a01f265f6c8bce7e59749ad914d0a9e0f6
danielwolfm <16danielwolf@gmail.com>
2019-02-21 19:00:12 +0100

    implemented binarization and edge detection with morphological filtering

--- src/main/java/preprocessing/Binarization.java
+++ src/main/java/preprocessing/Binarization.java
@@ -1,13 +1,30 @@
 package preprocessing;
 
 import com.bv_gruppe_d.imagej.ImageData;
+import ij.process.AutoThresholder;
+import ij.process.ImageProcessor;
 
 public class Binarization implements IPreprocessingOperation {
 
+	private static final int THRESHOLDINC = 128, WHITE = 0xFFFFFF, BLACK = 0x000000;
+
 	@Override
 	public ImageData execute(ImageData imageData) {
-		// TODO Auto-generated method stub
-		return null;
+		ImageProcessor imageProcessor = imageData.getImageProcessor();
+
+		//imageData.getImageProcessor().setAutoThreshold(AutoThresholder.Method.Mean, false);
+
+
+		for (int x = 0; x < imageProcessor.getWidth(); x++) {
+			for (int y = 0; y < imageProcessor.getHeight(); y++) {
+
+				int binarizedPixelValue = imageProcessor.get(x,y) >= THRESHOLDINC ? WHITE : BLACK;
+				imageProcessor.set(x, y, binarizedPixelValue);
+
+			}
+		}
+
+		return imageData;
 	}
 
 }
