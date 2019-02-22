fe3a8546da619d5de9a605e5a127794438ccdc21
MSandeman <msandermann96@gmail.com>
2019-02-22 23:45:26 +0100

    Add first draft of learning and testing algorithm
    Add conversion to libsvm datatypes
    Add create parameters for learning
    Add double representation to Label enum

--- src/main/java/classification/Classificator.java
+++ src/main/java/classification/Classificator.java
@@ -1,14 +1,74 @@
 package classification;
 
+import java.util.ArrayList;
+
 import com.bv_gruppe_d.imagej.Lable;
+
+import ij.IJ;
+import libsvm.svm;
+import libsvm.svm_model;
+import libsvm.svm_node;
+import libsvm.svm_parameter;
+import libsvm.svm_problem;
 
 public class Classificator {
 	
+	private svm_model model;
+	
 	public void learnClassifier(FeatureVector[] featureVectors) {
+		svm_parameter parameters = createParametrizationForLearning();
 		
+		svm_problem data = createDataFormatForLearning(featureVectors);
+
+		if(svm.svm_check_parameter(data, parameters) == null) {
+			IJ.showMessage("Klassifiziere wird trainiert.");
+			model = svm.svm_train(data, parameters);
+		}
+		
+		IJ.showMessage("Done.");
 	}
 	
-	public Lable testClassifier(FeatureVector featureVectors) {
-		return null;
+	private svm_parameter createParametrizationForLearning() {
+		svm_parameter parameters = new svm_parameter();
+		parameters.svm_type = svm_parameter.NU_SVC;
+		parameters.kernel_type = svm_parameter.POLY;
+		return parameters;
+	}
+
+	private svm_problem createDataFormatForLearning(FeatureVector[] featureVectors) {
+		svm_problem data = new svm_problem();
+		data.l = featureVectors.length; 						// Number of Trainingsdata		
+		data.y = new double[featureVectors.length];				// Labels 
+		ArrayList<svm_node[]> convertedFeatureVectors = new ArrayList<>();// Features in sparse representation
+		
+		// Create svm_nodes
+		for (int i = 0; i < featureVectors.length; i++) {
+			data.y[i] = featureVectors[i].getLable().getNumericRepresentation();
+			
+			svm_node[] features = new svm_node[featureVectors[i].getFeatureValues().length];
+			for (int j = 0; j < featureVectors[i].getFeatureValues().length; j++) {
+				svm_node feature = new svm_node();
+				feature.index = j;
+				feature.value = featureVectors[i].getFeatureValues()[j];
+				features[j] = feature;
+			}
+			convertedFeatureVectors.add(features);
+		}		
+		
+		data.x = (svm_node[][]) convertedFeatureVectors.toArray();		// Features in sparse representation
+		
+		return data;
+	}
+	
+	public Lable testClassifier(FeatureVector featureVector) {
+		svm_node[] features = new svm_node[featureVector.getFeatureValues().length];
+		for (int j = 0; j < featureVector.getFeatureValues().length; j++) {
+			svm_node feature = new svm_node();
+			feature.index = j;
+			feature.value = featureVector.getFeatureValues()[j];
+			features[j] = feature;
+		}
+		
+		return Lable.valueOf(svm.svm_predict(model, features));
 	}
 }
--- src/main/java/com/bv_gruppe_d/imagej/Lable.java
+++ src/main/java/com/bv_gruppe_d/imagej/Lable.java
@@ -1,10 +1,34 @@
 package com.bv_gruppe_d.imagej;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public enum Lable {
-	NO_STRETCH,
-	MEDIUM_STRETCH,
-	MAXIMUM_STRECH,
-	SHEARD,
-	DISTURBANCE,
-	UNKNOWN
+	NO_STRETCH(1.0),
+	MEDIUM_STRETCH(2.0),
+	MAXIMUM_STRECH(3.0),
+	SHEARD(4.0),
+	DISTURBANCE(5.0),
+	UNKNOWN(6.0);
+	
+    private double numericRepresentation;
+    private static Map representationMapping = new HashMap<>();
+
+    Lable(double numericRepresentation) {
+        this.numericRepresentation = numericRepresentation;
+    }
+
+    public double getNumericRepresentation() {
+        return numericRepresentation;
+    }
+    
+    static {
+        for (Lable label: Lable.values()) {
+            representationMapping.put(label.numericRepresentation, label);
+        }
+    }
+
+    public static Lable valueOf(double label) {
+        return (Lable) representationMapping.get(label);
+    }
 }
