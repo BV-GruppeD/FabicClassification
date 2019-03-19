3ce0a4f36ca4dd120c7cedb0b13af094c42790bf
six-two (lenovo) <send.spam.here.please@pm.me>
2019-03-19 16:32:43 +0100

    Recommitting Daniels changes

--- src/main/java/preprocessing/Binarization.java
+++ src/main/java/preprocessing/Binarization.java
@@ -1,26 +1,49 @@
 package preprocessing;
 
 import com.bv_gruppe_d.imagej.ImageData;
+import ij.process.AutoThresholder;
 import ij.process.ImageProcessor;
 
 //TODO comment
-public class Binarization {
+public abstract class Binarization {
 	private static final int THRESHOLDINC = 128, WHITE = 0xFFFFFF, BLACK = 0x000000;
 
-	public ImageData execute(ImageData imageData) {
+
+	/**
+	 *
+	 * @param imageData
+	 * @param methode 0 = selfimplementation, 1: Threshhold = Mean, 2: Threshhold = MaxEntropy
+	 * @return binarized ImageData object result depends on slected function
+	 */
+	public static ImageData execute(ImageData imageData, int methode) {
 		ImageProcessor imageProcessor = imageData.getImageProcessor();
 
-		//imageData.getImageProcessor().setAutoThreshold(AutoThresholder.Method.Mean, false);
 
+		switch (methode) {
 
-		for (int x = 0; x < imageProcessor.getWidth(); x++) {
-			for (int y = 0; y < imageProcessor.getHeight(); y++) {
+			case 0:
+				for (int x = 0; x < imageProcessor.getWidth(); x++) {
+					for (int y = 0; y < imageProcessor.getHeight(); y++) {
 
-				int binarizedPixelValue = imageProcessor.get(x,y) >= THRESHOLDINC ? WHITE : BLACK;
-				imageProcessor.set(x, y, binarizedPixelValue);
+						int binarizedPixelValue = imageProcessor.get(x,y) >= THRESHOLDINC ? WHITE : BLACK;
+						imageProcessor.set(x, y, binarizedPixelValue);
 
-			}
+					}
+				}
+				break;
+
+			case 1:
+				imageData.getImageProcessor().setAutoThreshold(AutoThresholder.Method.Mean, false);
+				break;
+
+			case 2:
+				imageData.getImageProcessor().setAutoThreshold(AutoThresholder.Method.MaxEntropy, true);
+
+			default:
+				break;
+
 		}
+
 
 		return imageData;
 	}
--- src/main/java/preprocessing/PreProcessing.java
+++ src/main/java/preprocessing/PreProcessing.java
@@ -3,16 +3,14 @@ package preprocessing;
 import com.bv_gruppe_d.imagej.ImageData;
 
 public class PreProcessing {
-	private final Binarization binarization;
 	private final MorphologicalFiltering edgeDetection;
 	
 	public PreProcessing() {
-		binarization = new Binarization();
 		edgeDetection = new MorphologicalFiltering();
 	}
 
 	public void execute(ImageData imageData) {
-		binarization.execute(imageData);
+		Binarization.execute(imageData, 2);
 		edgeDetection.execute(imageData);
 		//TODO make all preprocessing methods return void?I mean we never use the result and it is kind of confusing
 	}
