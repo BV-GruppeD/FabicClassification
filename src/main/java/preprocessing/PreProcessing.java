e4d74c7fde2ea373001a071a15911679f0488183
Daniel <16danielwolf@gmail.com>
2019-04-02 17:12:22 +0200

    added contrast adjustment

--- src/main/java/preprocessing/PreProcessing.java
+++ src/main/java/preprocessing/PreProcessing.java
@@ -8,6 +8,7 @@ public abstract class PreProcessing {
 	}
 
 	public static ImageData execute(ImageData imageData) {
+		ContrastAdjustment.execute(imageData);
 		Binarization.execute(imageData);
 		return MorphologicalFiltering.execute(imageData);
 		//TODO make all preprocessing methods return void?I mean we never use the result and it is kind of confusing
